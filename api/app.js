da0ea33fb6b73f7a8681e829d04786894858e882
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-11 09:46:46 -0300

    feat(api): feat(api): servir archivos estaticos y actualizar index.html path para deploy

--- api/app.js
+++ api/app.js
@@ -1,6 +1,11 @@
 import express, { json } from 'express';
 import { movieRouter } from '../routes/movies.js'; // Importa el enrutador de películas
 import { corsMiddleware } from '../middlewares/cors.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express(); // Instancia de Express
 const port = process.env.PORT ?? 3000; // Puerto del servidor, por variable de entorno o 3000
@@ -10,6 +15,8 @@ app.use(corsMiddleware()); // Middleware de CORS
 app.disable('x-powered-by'); // Deshabilita la cabecera X-Powered-By
 
 app.use('/api', movieRouter); // Monta el enrutador de películas en la ruta /api
+
+app.use(express.static(path.join(__dirname, '../public'))); // Middleware para servir archivos estáticos
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
