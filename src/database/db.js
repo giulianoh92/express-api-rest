a3152288f724118fec774888dcb3d4357a90d833
giulianoh92 <giulianohillebrand@gmail.com>
2024-11-11 13:56:42 -0300

    fix(db): actualizar la conexión a la base de datos para usar la URL de Heroku y habilitar SSL

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres'
-    }
-);
+// Utiliza la URL de conexión de Heroku directamente
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,  // Puedes activar esto para ver los logs de las consultas
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,  // Esto es necesario para conectar a Heroku Postgres
+        },
+    },
+});
+
+export { sequelize };
